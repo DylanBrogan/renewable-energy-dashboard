c0de88299b0a28c34b0736b8f1fc154827d8b2b9
DylanBrogan <wolfmano138@gmail.com>
2024-08-19 20:05:55 -0400

    Remove venv, restructure

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,13 @@
 FROM python:3.9-slim
 
-WORKDIR /dashboard
+# Use an official Node.js runtime as a parent image
+# FROM node:16
 
-COPY orm.py /dashboard/orm.py
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy package.json and package-lock.json files to the working directory
+COPY package*.json ./
 
 # Install SQLite3
 RUN apt-get update && \
@@ -10,7 +15,16 @@ RUN apt-get update && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
 
+RUN npm install
+
 # Install Python packages
 RUN pip install pyserial
+RUN pip install flask
 
-CMD ["python", "orm.py"] 
+COPY . .
+
+# Expose the port that the app will run on
+EXPOSE 3000
+
+# Start the application
+CMD ["npm", "start"]
---
+++ backend/orm.py
@@ -0,0 +1,50 @@
+# #!/usr/bin/env python3
+# import serial
+# import sqlite3
+
+# conn = sqlite3.connect('data/orm_test.db')
+# cr = conn.cursor()
+
+# def insert_test():
+#     if __name__ == '__main__':
+#         ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+#         ser.reset_input_buffer()
+    
+#         buffer=""
+#         data_started=False
+#         print("Program started, waiting for <start> tag\n")
+#         while True:
+#             chunk = ser.read(ser.in_waiting).decode('utf-8')
+    
+#             if chunk:
+#                 buffer += chunk
+#                 if not data_started:
+#                     # Look for the start tag
+#                     start_idx = buffer.find("<start>")
+#                     if start_idx != -1:
+#                         data_started = True
+#                         buffer = buffer[start_idx + len("<start>"):]  # Start processing after the start tag
+#                 else:
+#                     # Look for the end tag to process complete messages
+#                     end_idx = buffer.find("<end>")
+#                     if end_idx != -1:
+#                         lines = buffer[:end_idx].strip().splitlines()
+#                         for line in lines:
+#                             query="INSERT INTO ins_test (str, num) VALUES "
+#                             substr = line.split(',')
+#                             char = substr[0].strip()
+#                             num = substr[1].strip()
+#                             query += f'("{char}", {num})'
+#                             cr.execute(query)
+                            
+#                         conn.commit()
+#                         conn.close()
+#                         break
+    
+#                         # Optional: set data_started to False if you only want to process one message at a time
+               
+# def select_test():
+#     cr.execute("SELECT * FROM ins_test")
+#     return cr.fetchone()
+
+# print(select_test())
