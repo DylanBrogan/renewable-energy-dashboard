40a4e101a0135cc92438984b11be8d6edc084cc7
ckromer2 <2020ckromer@gmail.com>
2024-11-12 17:11:10 -0500

    Added the hydro to the frontend

--- chakra-nextjs-dashboard/pages/api/hello.js
+++ chakra-nextjs-dashboard/pages/api/hello.js
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
 
 
     // Query the database (example: select all users)
-    const users = await db.all('SELECT * FROM solar_data');
+    const users = await db.all('SELECT sd.create_date, sd.watts as solar_watts, sd.surface_temperature, sd.ambient_temperature, hd.watts as hydro_watts, hd.flowrate FROM solar_data sd JOIN hydro_data hd ON sd.create_date = hd.create_date');
 
     // Close the database connection
     await db.close();
