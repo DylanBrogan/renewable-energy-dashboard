159bf53380ae5635843dadd8f54e4a9781468f03
ckromer2 <2020ckromer@gmail.com>
2024-11-26 16:56:47 -0500

    Merge 2

--- chakra-nextjs-dashboard/pages/api/singleSelect.js
+++ chakra-nextjs-dashboard/pages/api/singleSelect.js
@@ -20,11 +20,7 @@ export default async function handler(req, res) {
 
 
     // Query the database (example: select all users)
-<<<<<<< HEAD
-    const data = await db.all('SELECT sd.create_date, sd.watts as solar_watts, sd.surface_temperature, sd.ambient_temperature, hd.watts as hydro_watts, hd.flowrate FROM solar_data sd JOIN hydro_data hd ON sd.create_date = hd.create_date ORDER BY sd.create_date DESC Limit 1');
-=======
     const users = await db.all('SELECT sd.create_date, sd.watts as solar_watts, sd.surface_temperature, sd.ambient_temperature, hd.watts as hydro_watts, hd.flowrate, wd.watts as wind_watts FROM solar_data sd JOIN hydro_data hd ON sd.create_date = hd.create_date JOIN wind_data wd ON sd.create_date = wd.create_date ORDER BY sd.create_date DESC Limit 1');
->>>>>>> 67a807ad523fccb0548fb8d460267212cd724069
 
     // Close the database connection
     await db.close();
